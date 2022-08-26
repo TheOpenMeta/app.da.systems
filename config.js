29fd627e96c51b76d03c1db5d8e662d6bae71b80
Calvin <developerleebo@gmail.com>
2022-08-26 18:55:11 +0800

    feat: change App name

--- config.js
+++ config.js
@@ -1,15 +1,16 @@
 const packageJson = require('./package.json')
-const isProdData = window.location.host === '127.0.0.1:21000'
-const hostname = isProdData ? '127.0.0.1:21000' : '127.0.0.1:21000'
+const { isProd } = require('./abc.config')
+const isProdData = isProd
+const hostname = isProdData ? 'bitxyz.io' : '127.0.0.1:21000'
 
 module.exports = {
   isProdData,
   appNmae: packageJson.name,
   hostname,
   domain: `https://${hostname}/`,
-  servicesApi: isProdData ? '' : '',
-  crossEthApi: isProdData ? '' : '',
-  identiconServe: '',
+  servicesApi: isProdData ? 'https://register-api.didname.jp/v1' : 'https://register-api.didname.jp/v1',
+  crossEthApi: isProdData ? 'https://main-cross-api.did.id/v1' : 'https://main-cross-api.did.id/v1',
+  identiconServe: 'https://identicons.did.id/identicon/',
   didtop: isProdData ? 'https://did.top' : 'https://test.did.top',
   homepage: isProdData ? 'https://data.did.id' : 'https://testdata.did.id',
   dasBalance: isProdData ? 'https://balance.did.id' : 'https://testbalance.did.id',
--- nuxt.config.js
+++ nuxt.config.js
@@ -12,27 +12,27 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: '.bit - Web3 identities for you and your community.',
+    title: 'Mybit - Web3 identities for you and your community.',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width,height=device-height,initial-scale=1.0,maximum-scale=1.0,user-scalable=no,viewport-fit=cover' },
       { name: 'apple-mobile-web-app-capable', content: 'yes' },
-      { hid: 'description', name: 'description', content: '.bit is a blockchain-based, decentralized and open sourced account system. It is the first account system with broad compatibility and complete ownership and control by users. .bit can be used as a cryptocurrency collection account, as a domain name, or as an account for accessing general Internet services.' },
-      { hid: 'keyword', name: 'keywords', content: 'domain, domain name, blockchain domain, nervos, ckb, .bit, domain name registration, purchase domain name, website domain, domain name service, domain name hosting, timyang, .bit, domain investment, DNS, buy domain name' },
+      { hid: 'description', name: 'description', content: 'Mybit is a blockchain-based, decentralized and open sourced account system. It is the first account system with broad compatibility and complete ownership and control by users. Mybit can be used as a cryptocurrency collection account, as a domain name, or as an account for accessing general Internet services.' },
+      { hid: 'keyword', name: 'keywords', content: 'domain, domain name, blockchain domain, nervos, ckb, Mybit, domain name registration, purchase domain name, website domain, domain name service, domain name hosting, timyang, Mybit, domain investment, DNS, buy domain name' },
       // The Open Graph protocol
-      { hid: 'og:description', property: 'og:description', content: '.bit is a blockchain-based, decentralized and open sourced account system. It is the first account system with broad compatibility and complete ownership and control by users. .bit can be used as a cryptocurrency collection account, as a domain name, or as an account for accessing general Internet services.' },
-      { hid: 'og:title', property: 'og:title', content: '.bit - Web3 identities for you and your community.' },
-      { hid: 'og:site_name', property: 'og:site_name', content: '.bit - Web3 identities for you and your community.' },
+      { hid: 'og:description', property: 'og:description', content: 'Mybit is a blockchain-based, decentralized and open sourced account system. It is the first account system with broad compatibility and complete ownership and control by users. Mybit can be used as a cryptocurrency collection account, as a domain name, or as an account for accessing general Internet services.' },
+      { hid: 'og:title', property: 'og:title', content: 'Mybit - Web3 identities for you and your community.' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'Mybit - Web3 identities for you and your community.' },
       { hid: 'og:type', property: 'og:type', content: 'article' },
       { hid: 'og:url', property: 'og:url', content: 'https://did.id/' },
       { hid: 'og:image', property: 'og:image', content: `https://${abcConfig.hostname}/images/seo-thumbnail-image.png` },
       // Twitter
       { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
-      { hid: 'twitter:site', name: 'twitter:site', content: '.bit - Web3 identities for you and your community.' },
-      { hid: 'twitter:title', name: 'twitter:title', content: '.bit - Web3 identities for you and your community.' },
-      { hid: 'twitter:description', name: 'twitter:description', content: '.bit is a blockchain-based, decentralized and open sourced account system. It is the first account system with broad compatibility and complete ownership and control by users. .bit can be used as a cryptocurrency collection account, as a domain name, or as an account for accessing general Internet services.' },
+      { hid: 'twitter:site', name: 'twitter:site', content: 'Mybit - Web3 identities for you and your community.' },
+      { hid: 'twitter:title', name: 'twitter:title', content: 'Mybit - Web3 identities for you and your community.' },
+      { hid: 'twitter:description', name: 'twitter:description', content: 'Mybit is a blockchain-based, decentralized and open sourced account system. It is the first account system with broad compatibility and complete ownership and control by users. Mybit can be used as a cryptocurrency collection account, as a domain name, or as an account for accessing general Internet services.' },
       { hid: 'twitter:image', name: 'twitter:image', content: `https://${abcConfig.hostname}/images/seo-thumbnail-image.png` },
-      { hid: 'twitter:image:alt', name: 'twitter:image:alt', content: '.bit - Web3 identities for you and your community.' }
+      { hid: 'twitter:image:alt', name: 'twitter:image:alt', content: 'Mybit - Web3 identities for you and your community.' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
